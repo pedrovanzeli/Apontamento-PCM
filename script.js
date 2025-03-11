3f7b50aeb018de91ed99eea596aaa6e0e602f5d8
pedrovanzeli <pedrovanzeli63@gmail.com>
2025-03-11 09:47:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 async function buscarSolicitacao() {
     const numeroSolicitacao = document.getElementById("numeroSolicitacao").value;
     const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQjHfFEDNbyJxmtlGysOJb3i35Oq217kDCjU8_4pGVpzMFeOA-qtbC2vV2d_4YG_tR9bcaTue2tr39M/pub?output=csv";
-    
     const response = await fetch(url);
     const data = await response.text();
     const rows = data.split("\n").map(row => row.split(","));
-    
     const headers = rows[0];
-    const solicitacao = rows.find(row => row[1] === numeroSolicitacao);
+    const solicitacao = rows.find(linha => linha[1] === numeroSolicitacao);
     
     if (solicitacao) {
         let html = "<h3>Detalhes da Solicitação</h3><ul>";
         headers.forEach((header, index) => {
-            if (!header.includes("Ordem Concluída") && !header.includes("Submission Date") && (!header.includes("LOCAL DE INSTALAÇÃO/EQUIPAMENTO") || solicitacao[index].trim() !== "")) {
+            if (!header.includes("Ordem Concluída") && !header.includes("Data de Submissão") && 
+                (!header.includes("LOCAL DE INSTALAÇÃO/EQUIPAMENTO") || solicitacao[index].trim() !== "")) {
                 html += `<li><strong>${header}:</strong> ${solicitacao[index]}</li>`;
             }
         });
@@ -35,6 +34,7 @@ function enviarApontamento() {
         observacao: document.getElementById("observacao").value,
         imagem: document.getElementById("imagem").files[0]
     };
+    
     console.log("Dados do Apontamento:", apontamento);
     alert("Apontamento enviado com sucesso!");
 }
